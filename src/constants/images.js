cbef54b864f1bab3d724f3a37ec5f258a04e2b9f
hafsachenni <hafsachenni77@gmail.com>
2024-09-23 20:25:52 +0100

    project setup completed

---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import cake from '../assets/cake.png';
+import glazed from '../assets/glazed.png';
+import bordure from '../assets/bordure.svg';
+
+export default {
+  cake,
+  glazed,
+  bordure,
+};
