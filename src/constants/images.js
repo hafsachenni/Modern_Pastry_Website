78660f15670a6b2df92bc014e91921a7e95efa24
hafsachenni <hafsachenni77@gmail.com>
2024-09-26 12:56:18 +0100

    responsive about us section done

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,11 +3,15 @@ import glazed from '../assets/glazed.png';
 import bordure from '../assets/bordure.svg';
 import pinkclr from '../assets/pink-bg.jpg';
 import gletter from '../assets/G.png';
+import borduredark from '../assets/borduredark.svg';
+import cake2 from '../assets/cake2.png';
 
 export default {
   cake,
   glazed,
   bordure,
   pinkclr,
-  gletter
+  gletter,
+  borduredark,
+  cake2
 };
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -5,11 +5,25 @@ import './AboutUs.css';
 const AboutUs = () => {
   return (
     <div id='about' className='app__aboutus app__bg flex__center section__padding'>
-      <div className='app__aboutus-overlay'>
-        <img src={images.gletter} alt='glazed overlay'/>
-      </div>
+      <div className='app__aboutus-content flex__center' > 
+        <div className='app__aboutus-content_about'>
+          <h1 className='headtext__cormorant'>About Us</h1>
+          <img src={images.borduredark} alt='bordure' className='spoon__img'/> 
+          <p className='p__opensans'> At Glazed, we create delectable pastries and cakes with love and care. Our mission is to bring joy through every bite, using only the finest ingredients to ensure quality.</p>
+          <button type='button' className='custom__button-bg'>Know More</button>
+        </div>
 
-      
+        <div className='app__aboutus-content_cake flex__center'>
+          <img src={images.cake2} alt='deco'/> 
+        </div>
+
+        <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+          <img src={images.borduredark} alt='bordure' className='spoon__img'/> 
+          <p className='p__opensans'> Glazed started as a small family bakery, built on passion and tradition. Today, we continue to bake with the same dedication, crafting sweets that bring happiness to our community.</p>
+          <button type='button' className='custom__button-bg'>Know More</button>
+        </div>
+      </div>
     </div>
   )
 }
