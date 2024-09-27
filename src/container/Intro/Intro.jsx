b235082e78ad3b9f3bd2eff825ebda106eb1b412
hafsachenni <hafsachenni77@gmail.com>
2024-09-27 14:08:57 +0100

    menu image changed

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+
 
 const Intro = () => {
+  const [isvideoplaying, setIsVideoPalying] = useState(false);
+
   return (
     <div>
       gty
