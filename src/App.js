e919dcd241e837c7eeeb0267f574593ed77f159c
hafsachenni <hafsachenni77@gmail.com>
2024-10-09 11:44:58 +0100

    loading spinner added + react router installed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
+import LoadingSpinner from './container/Spinner/LoadingSpinner';
 
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+
+  return (
+    <div>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <Navbar />
+          <Header />
+          <AboutUs />
+          <SpecialMenu />
+          <Chef />
+          <Intro />
+          <Laurels />
+          <Gallery />
+          <FindUs />
+          <Footer />
+        </>
+      )}
   </div>
 );
+};
 
 export default App;
